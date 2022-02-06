0d6a2cd387c928fe91fbde1e06575888c9931716
leejanghe <babybye777@gmail.com>
2022-02-06 22:57:19 +0900

    데이터 바인딩 test

---
+++ my-website/assets/data.js
@@ -0,0 +1,65 @@
+const studyData = [{
+    id : 0,
+    title: "Up Down Game",
+    subtitle:"11111",
+    image: "https://user-images.githubusercontent.com/75570030/152680146-b38248d7-2c70-4228-a975-e5e94af9162a.png",
+    github: "https://github.com/leejanghe/up_and_down_game",
+    site: "https://up-and-down-github-io.vercel.app/",
+    stack:"222222",
+    content : 'adsfasdf'
+    },
+    {
+    id : 1,
+    title: "Up Down Game",
+    subtitle:"123123",
+    image: "https://user-images.githubusercontent.com/75570030/152680146-b38248d7-2c70-4228-a975-e5e94af9162a.png",
+    github: "https://github.com/leejanghe/up_and_down_game",
+    site: "https://up-and-down-github-io.vercel.app/",
+    stack:"123",
+    content : 'adsfasdf'
+    },
+    // {
+    // id : 2,
+    // title: "Up Down Game",
+    // subtitle:"123123",
+    // image: "https://user-images.githubusercontent.com/75570030/152680146-b38248d7-2c70-4228-a975-e5e94af9162a.png",
+    // github: "https://github.com/leejanghe/up_and_down_game",
+    // site: "https://up-and-down-github-io.vercel.app/",
+    // stack:"123123",
+    // content : 'adsfasdf'
+    // },
+    // {
+    // id : 3,
+    // title: "Up Down Game",
+    // subtitle:"123123",
+    // image: "https://user-images.githubusercontent.com/75570030/152680146-b38248d7-2c70-4228-a975-e5e94af9162a.png",
+    // github: "https://github.com/leejanghe/up_and_down_game",
+    // site: "https://up-and-down-github-io.vercel.app/",
+    // stack:"123123",
+    // content : 'adsfasdf'
+    // },
+    // {
+    // id : 4,
+    // title: "Up Down Game",
+    // subtitle:"",
+    // image: "https://user-images.githubusercontent.com/75570030/152680146-b38248d7-2c70-4228-a975-e5e94af9162a.png",
+    // github: "https://github.com/leejanghe/up_and_down_game",
+    // site: "https://up-and-down-github-io.vercel.app/",
+    // stack:"",
+    // content : 'adsfasdf'
+    // },
+];
+
+const projectData =[
+    {
+      name: "kim Coding",
+      userImage: "https://placeimg.com/200/200/people",
+      postImage: "https://www.crescenthill.com/wp-content/uploads/2021/07/Banner-Image-188435-e1626372383514.jpg",
+      likes: 20,
+      date: "Apr 20",
+      liked: false,
+      content: "풍경을 페인팅 아트로 표현했어요! 너무 아름다워요!",
+    },
+  ]
+
+export { studyData, projectData };
--- my-website/pages/OtherStudy.js
+++ my-website/pages/OtherStudy.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import styles from '../styles/otherstudy.module.css'
+import Link from 'next/link'
+import {studyData} from '../assets/data.js'
+
 
 function OtherStudy() {
+
+    console.log(studyData)
+    
     return (
         <div className={styles.test}>
-            <h1>hello world!!</h1>
+            {
+                studyData.map((el)=>
+                <div key={el.id}>
+                    <h1>{studyData[0].title}</h1>
+                     <p>{studyData[0].subtitle}</p>
+                     <p>{studyData[0].stack}</p>
+                     <img src={studyData[0].image} width="30%"/>
+                     <Link href={studyData[0].github} ><a target="_blank">Github</a></Link>
+                     <Link href={studyData[0].site} ><a target="_blank">Github</a></Link>
+                      <p>{studyData[0].content}</p>
+                </div>
+                )
+            }
         </div>
     )
 }
