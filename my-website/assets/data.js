77eeda4ca70d11b1f76a6cd90f369c6e0e8107db
leejanghe <babybye777@gmail.com>
2022-02-07 04:41:18 +0900

    other study 그리드 및 데이터 바인딩

--- my-website/assets/data.js
+++ my-website/assets/data.js
@@ -10,44 +10,44 @@ const studyData = [{
     },
     {
     id : 1,
-    title: "Up Down Game",
+    title: "Paint",
     subtitle:"123123",
-    image: "https://user-images.githubusercontent.com/75570030/152680146-b38248d7-2c70-4228-a975-e5e94af9162a.png",
-    github: "https://github.com/leejanghe/up_and_down_game",
-    site: "https://up-and-down-github-io.vercel.app/",
+    image: "https://user-images.githubusercontent.com/75570030/152696814-c1b9c889-901a-4183-b510-a67c4098c5e6.png",
+    github: "https://github.com/leejanghe/paint",
+    site: "https://paint-five.vercel.app/",
     stack:"123",
     content : 'adsfasdf'
     },
-    // {
-    // id : 2,
-    // title: "Up Down Game",
-    // subtitle:"123123",
-    // image: "https://user-images.githubusercontent.com/75570030/152680146-b38248d7-2c70-4228-a975-e5e94af9162a.png",
-    // github: "https://github.com/leejanghe/up_and_down_game",
-    // site: "https://up-and-down-github-io.vercel.app/",
-    // stack:"123123",
-    // content : 'adsfasdf'
-    // },
-    // {
-    // id : 3,
-    // title: "Up Down Game",
-    // subtitle:"123123",
-    // image: "https://user-images.githubusercontent.com/75570030/152680146-b38248d7-2c70-4228-a975-e5e94af9162a.png",
-    // github: "https://github.com/leejanghe/up_and_down_game",
-    // site: "https://up-and-down-github-io.vercel.app/",
-    // stack:"123123",
-    // content : 'adsfasdf'
-    // },
-    // {
-    // id : 4,
-    // title: "Up Down Game",
-    // subtitle:"",
-    // image: "https://user-images.githubusercontent.com/75570030/152680146-b38248d7-2c70-4228-a975-e5e94af9162a.png",
-    // github: "https://github.com/leejanghe/up_and_down_game",
-    // site: "https://up-and-down-github-io.vercel.app/",
-    // stack:"",
-    // content : 'adsfasdf'
-    // },
+    {
+    id : 2,
+    title: "Art Shop Ground",
+    subtitle:"123123",
+    image: "https://user-images.githubusercontent.com/75570030/147211156-0947abe7-7b23-4bfa-a766-312a5736fea1.png",
+    github: "https://github.com/leejanghe/Art_Shop_ground_vue",
+    site: "https://art-ground-shop-github-io.vercel.app/",
+    stack:"123123",
+    content : 'adsfasdf'
+    },
+    {
+    id : 3,
+    title: "Youtube Clone",
+    subtitle:"123123",
+    image: "https://user-images.githubusercontent.com/75570030/152698111-1b4ae533-2d9e-4a7b-9c54-2ddc45402555.png",
+    github: "https://github.com/leejanghe/youtube_clone",
+    site: "https://youtube-clone-cyan-beta.vercel.app/",
+    stack:"123123",
+    content : 'adsfasdf'
+    },
+    {
+    id : 4,
+    title: "Starbucks Clone",
+    subtitle:"123123123",
+    image: "https://user-images.githubusercontent.com/75570030/152698188-91a456e5-c65e-4758-a8c3-48d9f5f44c9e.png",
+    github: "https://github.com/leejanghe/starbucks-cloncoding",
+    site: "https://starbucks-cloncoding.vercel.app/",
+    stack:"123123123",
+    content : 'adsfasdf'
+    },
 ];
 
 const projectData =[
--- my-website/pages/OtherStudy.js
+++ my-website/pages/OtherStudy.js
@@ -2,27 +2,41 @@ import React from 'react'
 import styles from '../styles/otherstudy.module.css'
 import Link from 'next/link'
 import {studyData} from '../assets/data.js'
-
+import {BsGithub} from 'react-icons/bs'
+import {CgWebsite} from 'react-icons/cg'
 
 function OtherStudy() {
 
     console.log(studyData)
     
     return (
-        <div className={styles.test}>
-            {
+        <div className={styles.study_bg}>
+            <h1 className={styles.study_title}>Other Study</h1>
+            <div className={styles.study_boxbg}>
+            {studyData &&
                 studyData.map((el)=>
-                <div key={el.id}>
-                    <h1>{studyData[0].title}</h1>
-                     <p>{studyData[0].subtitle}</p>
-                     <p>{studyData[0].stack}</p>
-                     <img src={studyData[0].image} width="30%"/>
-                     <Link href={studyData[0].github} ><a target="_blank">Github</a></Link>
-                     <Link href={studyData[0].site} ><a target="_blank">Github</a></Link>
-                      <p>{studyData[0].content}</p>
+                <div className={styles.study_box} key={el.id}>
+                    
+                        <img src={el.image} width="30%"/>
+       
+                    <div className={styles.study_container}>
+                    <div className={styles.study_header}>
+                        <h1>{el.title}</h1>
+                        <p>{el.subtitle}</p>
+                        <p>{el.stack}</p>
+                    </div>
+                    <div>
+                        <Link href={el.github} ><a target="_blank">Github <BsGithub/></a></Link>
+                        <Link href={el.site} ><a target="_blank">website <CgWebsite/></a></Link>
+                    </div>
+                    <div>
+                        <p>{el.content}</p>
+                    </div>
+                    </div>
                 </div>
                 )
             }
+            </div>
         </div>
     )
 }
