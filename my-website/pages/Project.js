966dc8fa31c95e020f6c7240e8d4b71d0f841d70
leejanghe <babybye777@gmail.com>
2022-02-07 14:41:16 +0900

    project 케러셀 및 셋팅

--- my-website/pages/Project.js
+++ my-website/pages/Project.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import styles from '../styles/project.module.css'
+import {projectData} from '../assets/data.js'
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Project() {
+
+    const settings = {
+        autoplay: true,
+        autoplaySpeed: 3000,
+        Infinity: true,
+        dots:true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    }
+
+
+    console.log(projectData)
+
     return (
         <div className={styles.test}>
-            <h1>projcet</h1>
+            <h1>projcet!!!</h1>
+            <Slider {...settings}>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>5</div>
+            </Slider>
         </div>
     )
 }
